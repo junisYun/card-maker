3ab28491589aaf366b361e56f5f45e87e2844c8b
컴퓨터 하는 라따뚜이 <81808703+junisYun@users.noreply.github.com>
2021-09-11 13:16:51 +0900

    add loading spinner for file input

--- src/components/Image_file_input/ImageFileInput.jsx
+++ src/components/Image_file_input/ImageFileInput.jsx
@@ -1,13 +1,16 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 import styles from './ImageFileInput.module.css';
 const ImageFileInput = ({ imageUploader, name, onFileChange }) => {
+  const [loading, setLoading] = useState(false);
   const inputRef = useRef();
   const onButtonClick = (event) => {
     event.preventDefault();
     inputRef.current.click();
   };
   const onChange = async (event) => {
+    setLoading(true);
     const upload = await imageUploader.upload(event.target.files[0]);
+    setLoading(false);
     console.log(upload);
     onFileChange({
       name: upload.original_filename,
@@ -24,9 +27,18 @@ const ImageFileInput = ({ imageUploader, name, onFileChange }) => {
         name="file"
         onChange={onChange}
       />
-      <button className={styles.button} onClick={onButtonClick}>
-        {name || 'No File'}
-      </button>
+      {!loading && (
+        <button
+          className={
+            name ? `${styles.button} ${styles.pink}` : `${styles.button}`
+          }
+          onClick={onButtonClick}
+        >
+          {name || 'No File'}
+        </button>
+      )}
+
+      {loading && <div className={styles.loading}></div>}
     </div>
   );
 };
